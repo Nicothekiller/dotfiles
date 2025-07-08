1d0d25b274206312dba5d63fb5a9f8808fe82f42
Nicothekiller <nicolasnaran@gmail.com>
2025-07-07 19:11:59 -0500

    added fzf pacman browse functions

--- dot_zsh/funcs.zsh
+++ dot_zsh/funcs.zsh
@@ -17,3 +17,11 @@ function lazyg() {
 function krn(){
 	kitten @ set-tab-title "$1"
 }
+
+function pacbrowse(){
+	pacman -Slq | fzf --multi --preview 'pacman -Si {1}'
+}
+
+function yaybrowse(){
+	yay -Slq | fzf --multi --preview 'yay -Si {1}'
+}
